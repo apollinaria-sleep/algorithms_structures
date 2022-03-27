fe5aa83aa5354a3d5a1a853ad260afa14559d8a2
apollinaria-sleep <60830365+apollinaria-sleep@users.noreply.github.com>
2022-03-27 20:00:28 +0300

    Add files via upload

---
+++ MemoryManager/MemoryManagerTest.cpp
@@ -0,0 +1,33 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "mm1.h"
+#include "doctest.h"
+
+struct TwoInt {
+    int first = 0;
+    int second = 1;
+    TwoInt() = default;
+    TwoInt(int f, int s) : first(f), second(s) {}
+};
+
+
+TEST_CASE("one_block") {
+    lab618::CMemoryManager<TwoInt> mm(10, false);
+    TwoInt* obj = mm.newObject();
+    CHECK(mm.deleteObject(obj) == true);
+}
+
+
+TEST_CASE("two_blocks") {
+    lab618::CMemoryManager<TwoInt> mm(3, false);
+    TwoInt* obj1 = mm.newObject();
+    TwoInt* obj2 = mm.newObject();
+    CHECK(obj1 != obj2);
+    TwoInt* obj3 = mm.newObject();
+    CHECK(obj2 != obj3);
+    TwoInt* obj4 = mm.newObject();
+    CHECK(obj3 != obj4);
+    mm.deleteObject(obj1);
+    mm.deleteObject(obj2);
+    mm.deleteObject(obj3);
+    CHECK(mm.deleteObject(obj4) == true);
+}
